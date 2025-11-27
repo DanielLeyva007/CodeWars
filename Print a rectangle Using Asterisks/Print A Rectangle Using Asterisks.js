cf795f1b4a1f4cd49c5a923224bb9cf7b00fff8e
DanielLeyva007 <dannyl77u@gmail.com>
2025-11-27 07:19:53 -0800

    added reverse negative code war

---
+++ Print a rectangle Using Asterisks/Print A Rectangle Using Asterisks.js
@@ -0,0 +1,24 @@
+//Write a method that, given two arguments, width and height, returns a string representing a rectangle with those dimensions.
+
+//The rectangle should be filled with spaces, and its borders should be composed of asterisks (*).
+
+//For example, given width = 3 and height = 3:
+
+//***
+//* *
+//***
+//End each line of the string (including the last one) with a carriage return-line feed combination.
+
+//Note: You may assume that width and height will always be greater than zero.
+
+
+
+const getRectangleString = (w, h) => 
+	Array.from({ length: h }, (_, i) => 
+  	(!i || i === h - 1 ? 
+    	'*'.repeat(w) : 
+      '*' + ' '.repeat(w - 2) + '*'
+    ) + '\r\n'
+  ).join('');
+  	
+
